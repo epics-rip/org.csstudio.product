485b06ed0df0163472f5095101ed953a58eb55da
Eric Berryman <eric.berryman@gmail.com>
2014-08-15 15:04:13 -0400

    prepare-next-release.sh: removed roll over from 3.3.9 to 3.4.0

--- prepare-next-release.sh
+++ prepare-next-release.sh
@@ -5,23 +5,7 @@ VERSION=$1
 PUSH=$2
 BUILD_DIR="build"
 
-increment_version ()
-{
-  declare -a part=( ${1//\./ } )
-  declare    new
-  declare -i carry=1
-
-  for (( CNTR=${#part[@]}-1; CNTR>=0; CNTR-=1 )); do
-    len=${#part[CNTR]}
-    new=$((part[CNTR]+carry))
-    [ ${#new} -gt $len ] && carry=1 || carry=0
-    [ $CNTR -gt 0 ] && part[CNTR]=${new: -len} || part[CNTR]=${new}
-  done
-  new="${part[*]}"
-  echo -e "${new// /.}"
-} 
-
-VERSIONP=$(increment_version ${VERSION})
+VERSIONP=$(echo $VERSION | sed -r 's/(.*)\.(.*)\.(.*)/echo \1\.\2\.$((\3+1))/ge');
 VERSION="${VERSIONP}-SNAPSHOT"
 
 if [! $# == 1 ]
